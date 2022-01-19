0e63d8c47ff60505c2d72eed6bf2c514e6ecdb0a
Navemics <navemics@gmail.com>
2022-01-19 14:08:31 +0530

    Initial Commit

---
+++ Musixzam/MusixzamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusixzamApp.swift
+//  Musixzam
+//
+//  Created by NAVEEN MADHAN on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MusixzamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
